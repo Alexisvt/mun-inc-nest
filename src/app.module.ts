5100e65444e86121a5331cd38f3239c665a61e28
Alexis Villegas Torres <alexisvt@gmail.com>
2020-04-03 01:15:33 -0600

    feat(App): add config module
    Add ConfigModule to use environment variables.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
@@ -6,11 +7,23 @@ import { AppService } from './app.service';
 import { MunModule } from './mun/mun.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://mongo:27017/mun'), MunModule],
-  // imports: [
-  //   MongooseModule.forRoot('mongodb://localhost:27017/mun'),
-  //   CatsModule,
-  // ],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.${process.env.NODE_ENV || 'development'}`],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: [`.env.${process.env.NODE_ENV || 'development'}`],
+        }),
+      ],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_CONNECTION'),
+      }),
+      inject: [ConfigService],
+    }),
+    MunModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
